97509faa7868a44de0c097b5e1f9654df3399808
woodvase <yongcai.liu@outlook.com>
2015-06-19 22:08:43 -0700

    Interval.cs

---
+++ InterviewQs/Interval.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: yoli
+ * Date: 6/19/2015
+ * Time: 10:05 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace InterviewQs
+{
+	/// <summary>
+	/// Description of Interval.
+	/// </summary>
+	public class Interval
+	{
+		public int start;
+		public int end;
+		public Interval()
+		{
+			this.start = 0;
+			this.end = 0;
+		}
+		public Interval(int start, int end)
+		{
+			this.start = start;
+			this.end = end;
+		}
+	}
+}
