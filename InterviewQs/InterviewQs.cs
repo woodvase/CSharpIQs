e872807645e93f9b9fed8d22223406a8755943ce
woodvase <yongcai.liu@outlook.com>
2015-07-13 22:14:12 -0700

    Q 236

--- InterviewQs/InterviewQs.cs
+++ InterviewQs/InterviewQs.cs
@@ -51,8 +51,22 @@ namespace InterviewQs
 			Console.ReadKey(true);
 		}
 		
-		// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+		// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
 		public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+		{
+			if (root == null || root == p || root == q)
+				return root;
+			TreeNode left = LowestCommonAncestor(root.left, p, q);
+			TreeNode right = LowestCommonAncestor(root.right, p, q);
+			if (left != null && right != null)
+				return root;
+			if (left == null)
+				return right;
+			return left;
+		}
+		
+		// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+		public TreeNode LowestCommonAncestorBST(TreeNode root, TreeNode p, TreeNode q)
 		{
 			if (root == null)
 				return null;
