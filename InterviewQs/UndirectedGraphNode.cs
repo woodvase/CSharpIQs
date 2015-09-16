309a9f6c6dd8f89c5b26db7346800d3ff2cc8137
woodvase <yongcai.liu@outlook.com>
2015-09-15 22:27:25 -0700

    UndirectedGraphNode class

---
+++ InterviewQs/UndirectedGraphNode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InterviewQs
+{
+    public class UndirectedGraphNode
+    {
+        public int label;
+        public IList<UndirectedGraphNode> neighbors;
+        public UndirectedGraphNode(int x) { label = x; neighbors = new List<UndirectedGraphNode>(); }
+    }
+}
